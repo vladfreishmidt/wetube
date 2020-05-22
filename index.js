135987e90bdab34db67babfafff75301b2238fef
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-05-22 14:18:38 +0300

    Midleware Morgan(logger) Helmet(security) were added

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on http://localhost:${PORT}`);
 
+const handleHome = (req, res, next) => res.send('Hello from my ass');
 
-const handleHome = (req, res) => res.send('Hello from my ass');
+const handleProfile = (req, res, next) => res.send("You are on my profile!");
 
 
-const handleProfile = (req, res) => res.send("You are on my profile!");
 
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.get('/', handleHome);
 
-app.get('/profile', handleProfile)
+app.get('/profile', handleProfile);
 
 app.listen(PORT, handleListening);
