74a8a326e3438965e001707ce3b3ff4988ff7662
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-05-22 18:20:12 +0300

    cookie-parser body-parser were added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -13,8 +16,11 @@ const handleProfile = (req, res, next) => res.send("You are on my profile!");
 
 
 
-app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(morgan("dev"));
 
 app.get('/', handleHome);
 
