927324017815193d1b13ef261781983d0a75207d
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-05-28 13:17:30 +0300

    Added partials to re use header/footer with PUG

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Details" });
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
