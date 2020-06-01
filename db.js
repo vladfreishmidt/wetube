4e6c4afbf65bfb8a6175bc0539f9b4b3cb707f90
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-06-01 23:43:35 +0300

    Home Controller and Join Controller were improved

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id: 323243,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator: {
+            id: 121212,
+            name: "Vlad",
+            email: "vladf@gmail.com"
+        }
+    },
+    {
+        id: 121212,
+        title: 'Video perfect',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator: {
+            id: 121212,
+            name: "Vlad",
+            email: "vladf@gmail.com"
+        }
+    },
+    {
+        id: 131313,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator: {
+            id: 121212,
+            name: "Vlad",
+            email: "vladf@gmail.com"
+        }
+    }
+];
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -5,16 +5,20 @@ import {
     search
 } from "../controllers/videoController";
 import {
-    join,
+    getJoin,
+    postJoin,
     login,
     logout
 } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
